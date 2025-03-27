d2d14fed1566c8336542389bc3f8a43ca0fe5ea7
Nagarathna S <166543180+NagarathnaTechie@users.noreply.github.com>
2025-03-27 20:43:17 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
+
+        
         <header>
             <div className="container">
                 <Link to = "/">
